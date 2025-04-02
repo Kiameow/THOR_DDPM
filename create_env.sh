e4d6b785c7a1e1f2b209b777a83aef7bfde8b5ac
Kiameow <zjzootl@qq.com>
2025-04-02 10:52:03 +0800

    modified script

--- create_env.sh
+++ create_env.sh
@@ -1,4 +1,4 @@
-conda create --name thor_test python=3.8.0
+conda create --name thor python=3.8.0
 # conda init
 # conda activate thor
 # pip3 install torch==1.9.1+cu111 torchvision==0.10.1+cu111 -f https://download.pytorch.org/whl/torch_stable.html
